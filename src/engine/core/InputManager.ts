107b1c738215f9a52eb50535eb88779d19cfece4
Anh Vũ <thomas.maihoanganhvu@gmail.com>
2022-06-06 17:47:04 +0700

    Add new input manager

--- src/engine/core/InputManager.ts
+++ src/engine/core/InputManager.ts
@@ -34,3 +34,21 @@ class InputManager {
     this._inputStack = [];
   }
 }
+
+class NewInputManager {
+  keys: string[];
+  constructor() {
+    this.keys = [];
+    window.addEventListener("keydown", (e) => {
+      if (
+        (e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "Space") &&
+        this.keys.indexOf(e.key) == -1
+      )
+        this.keys.push(e.key);
+    });
+    window.addEventListener("keyup", (e) => {
+      if (e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "Space")
+        this.keys.splice(this.keys.indexOf(e.key), 1);
+    });
+  }
+}
